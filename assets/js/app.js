9f88f11614f6c826d42850fa551ee94e564d1dfb
gianmarcodiale <gianmarcodialessandro@gmail.com>
2022-04-22 13:53:38 +0200

    add li dinamically

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,30 @@ const app = new Vue(
     {
         el: '#app',
         data: {
-            
+            newTask: '',
+            toDo: [
+                {
+                    text: 'Iscriversi in palestra',
+                    done: false
+                },
+                {
+                    text: 'Andare a fare la spesa',
+                    done: true
+                },
+                {
+                    text: 'Andare in banca',
+                    done: false
+                },
+                {
+                    text: 'Comprare un\'aspirapolvere nuovo',
+                    done: false
+                }
+            ]
+        },
+        methods: {
+            addTask() {
+                
+            }
         }
     }
 )
